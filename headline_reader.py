484e85a9214c0e4ba487a900fa61955e83b897dd
Arber Muharemi <arber.muharemi@gatech.edu>
2017-01-11 15:10:58 -0500

    added continue and stop app intents

--- headline_reader.py
+++ headline_reader.py
@@ -20,5 +20,16 @@ def launch_program():
     open_message = "Hello! Want to hear the news headlines?"
     return question(open_message)
 
+@ask.intent("Continue")
+def speak_headlines():
+    news_headlines = get_headlines()
+    news_msg = "These are the top news stories: {}".fromat(news_headlines)
+    return statement(news_msg)
+
+@ask.intent("Stop")
+def exit_program():
+    exit_message = "Okay, thanks for stopping by. See you soon!"
+    return statement(exit_message)
+
 if __name__ == '__main__':
     app.run(debug = True)
